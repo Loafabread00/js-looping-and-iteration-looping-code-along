47f434492a29201c5ee8851db720dcbf88b21ec8
sophie <sophie.wads@gmail.com>
2021-11-02 10:43:33 +1100

    Building a for and while loop

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(a,b) {
+    let name = [];
+    for (let i = 0; i < a.length; i++) {
+        name.push(`Thank you, ${ a[i]}, for the wonderful ${b} gift!`);
+    }
+    return name;
+}
+
+function countDown(n) {
+    for (let i = n; i >= 0; i--){
+        console.log(i);
+    }
+}
+
+//console.log(writeCards(["Lisa", "Kaitlin", "Jan"], "suprise"));
+// console.log(countDown(4));
